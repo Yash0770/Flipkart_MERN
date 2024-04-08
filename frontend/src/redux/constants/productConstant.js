8b669a86ed928a195864bd7c0ffd0851b5ce2435
DESKTOP-P9JEJPT\user7 <yashsolanki0728@gmail.com>
2024-04-08 11:16:13 +0530

    changes

--- frontend/src/redux/constants/productConstant.js
+++
@@ -1,7 +0,0 @@
-export const GET_PRODUCTS_SUCCESS = "getProductsSuccess";
-export const GET_PRODUCTS_FAILURE = "getProductsFailure";
-
-export const GET_PRODUCT_DETAILS_REQUEST = 'getProductDetailRequest';
-export const GET_PRODUCT_DETAILS_SUCCESS = 'getProductDetailSuccess';
-export const GET_PRODUCT_DETAILS_FAILURE = 'getProductDetailFailure';
-export const GET_PRODUCT_DETAILS_RESET = 'getProductDetailReset';
