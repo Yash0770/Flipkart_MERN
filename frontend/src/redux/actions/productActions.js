13d13b150bd2d1b805aab21586642e1589eadbcb
DESKTOP-P9JEJPT\user7 <yashsolanki0728@gmail.com>
2024-04-08 13:00:38 +0530

    add apis live url

--- frontend/src/redux/actions/productActions.js
+++ frontend/src/redux/actions/productActions.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 import * as actionTypes from "../constants/productConstant";
 
-const URL = "https://demo-mu-blue.vercel.app";
+const URL = "https://flipkart-backend-mern.vercel.app/";
 // const URL = "";
 
 export const getProducts = () => async (dispatch) => {
--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const URL = "http://localhost:7000";
-const URL = "https://demo-mu-blue.vercel.app";
+const URL = "https://flipkart-backend-mern.vercel.app/";
 // const URL = "";
 
 export const authenticateSignup = async (data) => {
